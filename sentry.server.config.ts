5edd5caba0525eefe1c8005ac4c6eaefe8381d3a
Yaroslav Yaroshovets <whiterainy76@gmail.com>
2025-03-27 17:13:05 +0200

    chore: remove next auth

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -8,12 +8,9 @@ Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT,
 
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-
-  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
 })
