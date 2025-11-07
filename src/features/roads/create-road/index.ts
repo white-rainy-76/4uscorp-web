da082e3f5a1c96298e5647dfc8eadb6c4f055241
white-rainy-76 <whiterainy76@gmail.com>
2025-11-07 02:42:12 +0200

    feat: add create-toll road feature

--- src/features/roads/create-road/index.ts
+++ src/features/roads/create-road/index.ts
@@ -1,2 +1,5 @@
+export { TollRoadEditor } from './ui/toll-road-editor'
+export { TollRoadEditorControls } from './ui/toll-road-editor-controls'
+// Legacy exports (deprecated)
 export { RoadEditor } from './ui/road-editor'
 export { RoadEditorControls } from './ui/road-editor-controls'
